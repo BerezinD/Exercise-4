322abf2e03aef4cad1279cd475c32ea1322301ce
BerezinD <baron_6600@hotmail.com>
2018-10-23 11:50:48 +0300

    First commit. UI only.

---
+++ app/src/main/java/com/example/exercise4/AsyncTaskActivity.kt
@@ -0,0 +1,12 @@
+package com.example.exercise4
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AsyncTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_async_task)
+    }
+}
